eca38e33d02f4262dd4788ef1480663ecb95bfe9
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-10-29 19:57:40 +0100

    Add setuptools configuration files with Cython support

---
+++ hmmer/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "MIT"
+__version__ = "0.1.0"
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+import configparser
+import glob
+import os
+import sys
+
+import setuptools
+from Cython.Build import cythonize
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
+
+
+class sdist(_sdist):
+    """A `sdist` that generates a `pyproject.toml` on the fly.
+    """
+
+    def run(self):
+        # build `pyproject.toml` from `setup.cfg`
+        c = configparser.ConfigParser()
+        c.add_section("build-system")
+        c.set("build-system", "requires", str(self.distribution.setup_requires))
+        c.set("build-system", 'build-backend', '"setuptools.build_meta"')
+        with open("pyproject.toml", "w") as pyproject:
+            c.write(pyproject)
+        # run the rest of the packaging
+        _sdist.run(self)
+
+
+class build_ext(_build_ext):
+    """A `build_ext` that disables optimizations if compiled in debug mode.
+    """
+
+    def build_extension(self, ext):
+        if self.debug:
+            if sys.platform == "linux" or sys.platform == "darwin":
+                ext.extra_compile_args.append("-O0")
+            if sys.implementation.name == "cpython":
+                ext.define_macros.append(("CYTHON_TRACE_NOGIL", 1))
+        _build_ext.build_extension(self, ext)
+
+
+extensions = [
+    setuptools.Extension(
+        "hmmer._hmmer",
+        [
+        ],
+        include_dirs=[
+            os.path.join("vendor", "hmmer"), 
+            os.path.join("vendor", "easel"),
+        ],
+        libraries=[],
+        library_dirs=[],
+        extra_compile_args=["-Wall"],
+    ),
+]
+
+
+setuptools.setup(
+    ext_modules=cythonize(extensions, annotate=True),
+    cmdclass=dict(sdist=sdist, build_ext=build_ext),
+)
