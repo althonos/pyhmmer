a34fa73256b7990eae58c0fcd8081fe00a717e21
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-02 21:56:33 +0100

    Make sure to build C libs in debug mode in CI

--- ci/travis/manylinux/_script.sh
+++ ci/travis/manylinux/_script.sh
@@ -8,8 +8,8 @@ export LIBRARY_PATH="$LIBRARY_PATH:$PYTHON_LIB"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PYTHON_LIB"
 
 cd /io
-$PYTHON_SYS_EXECUTABLE setup.py build_ext --inplace --debug
-$PYTHON_SYS_EXECUTABLE -m coverage run -p -m unittest discover -v
-$PYTHON_SYS_EXECUTABLE -m coverage combine
+$PYTHON_SYS_EXECUTABLE setup.py build_clib --debug
+$PYTHON_SYS_EXECUTABLE setup.py build_ext --debug --inplace
+$PYTHON_SYS_EXECUTABLE -m coverage run -m unittest discover -v
 $PYTHON_SYS_EXECUTABLE -m coverage xml
 $PYTHON_SYS_EXECUTABLE -m coverage report
