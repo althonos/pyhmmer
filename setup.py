ec65ea4f88ec200e0ac426f605d6b0e6d17f5bd2
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-02 19:42:04 +0100

    Rewrite `setup.py` to build libraries using `build_clib`

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ import shlex
 
 import setuptools
 from Cython.Build import cythonize
+from distutils import log
+from distutils.command.clean import clean as _clean
 from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.build_clib import build_clib as _build_clib
 from setuptools.command.sdist import sdist as _sdist
 
 
@@ -65,6 +68,13 @@ class build_ext(_build_ext):
 
 
     def build_extension(self, ext):
+        # ensure the required libraries have been built
+        cmd = self.get_finalized_command("build_clib")
+        cmd.run()
+
+        # update the extension C flags to use the temporary build folder
+        ext.library_dirs.extend(cmd.library_dirs)
+        ext.include_dirs.extend(cmd.include_dirs)
 
         # update compile flags if compiling in debug mode
         if self.debug:
@@ -73,46 +83,82 @@ class build_ext(_build_ext):
             if sys.implementation.name == "cpython":
                 ext.define_macros.append(("CYTHON_TRACE_NOGIL", 1))
 
-        # use the distutils/setuptools temporary folder to build out of source
-        _vendor_dir = os.path.join(os.path.dirname(__file__), "vendor")
-        _build_dir = os.path.join(self.build_temp, "hmmer")
-
-        # remove the build dir if rebuild is forced
-        if self.force and os.path.exists(os.path.join(_build_dir, "hmmer")):
-            self.remove_tree(os.path.join(_build_dir, "hmmer"))
-
-        # copy the HMMER and Easel source to the build directory
-        if not os.path.exists(_build_dir):
-            self.copy_tree(os.path.join(_vendor_dir, "hmmer"), _build_dir, preserve_symlinks=1)
-            self.copy_tree(os.path.join(_vendor_dir, "easel"), os.path.join(_build_dir, "easel"), preserve_symlinks=1)
-
-        # build the static libraries
-        cflags = ext.extra_compile_args
-        self.build_static("libhmmer.a", _build_dir, "src", cflags)
-        self.build_static("libeasel.a", os.path.join(_build_dir, "easel"), ".", cflags)
-
-        # update the extension C flags to use the temporary build folder
-        ext.library_dirs.append(os.path.join(_build_dir, "easel"))
-        ext.library_dirs.append(os.path.join(_build_dir, "src"))
-        ext.include_dirs.append(os.path.join(_build_dir, "easel"))
-        ext.include_dirs.append(os.path.join(_build_dir, "src"))
-
+        # build the rest of the extension as normal
         _build_ext.build_extension(self, ext)
 
 
-class clean_ext(setuptools.Command):
+class build_clib(_build_clib):
+    """A custom `build_clib` that compiles out of source.
+    """
 
-    user_options = [
-        ("all", "a", "remove all build output, not just temporary by-products"),
-    ]
+    def copy(self, libraries):
+        # copy the library sources to the build directory
+        for libname, info in self.libraries:
+            src = os.path.join(os.path.dirname(__file__), info["vendordir"])
+            dst = os.path.join(self.build_temp, info["builddir"])
+            if not os.path.exists(os.path.join(self.build_temp, info["makedir"])):
+                self.copy_tree(src, dst, preserve_symlinks=1)
 
-    def initialize_options(self):
-        self.all = False
+    def configure(self, libraries):
+        for lib, info in libraries:
+            # update environ to invoke `./configure`, which will involve an
+            # `os.chdir` eventually.
+            _env = os.environ.copy()
+            _cflags = " ".join(self.compiler.compiler[1:])
+            _cwd = os.getcwd()
 
-    def finalize_options(self):
-        pass
+            try:
+                # pass parameters from `self.compiler` to the `configure` script
+                # using environment variables
+                os.environ["AR"] = self.compiler.archiver[0]
+                os.environ["CC"] = self.compiler.compiler[0]
+                os.environ["CFLAGS"] = _cflags
+
+                # chdir to the directory where to run autoconf
+                build_dir = os.path.join(self.build_temp, info["builddir"])
+                log.info("entering directory {!r}".format(build_dir))
+                os.chdir(build_dir)
+
+                # run autoconf to generate the configure script
+                self.make_file(["configure.ac"], "configure", self.spawn, (["autoconf"],))
+
+                # run the configure script
+                configure_cmd = ["./configure", "--enable-pic", "--enable-threads"]
+                if self.debug:
+                    configure_cmd.append("--enable-debugging")
+                self.make_file(["configure"], "Makefile", self.spawn, (configure_cmd,))
+
+            finally:
+                os.environ.update(_env)
+                os.chdir(_cwd)
+
+    def make(self, libraries):
+        for lib, info in libraries:
+            makedir = os.path.join(self.build_temp, info["makedir"])
+            libname = self.compiler.library_filename(lib)
+            self.spawn(["make", "-C", makedir, libname])
+
+    def build_libraries(self, libraries):
+        # copy sources and build libraries using autoconf/configure/make
+        self.copy(libraries)
+        self.configure(libraries)
+        self.make(libraries)
+
+        # setup the library dirs and include dirs used by `build_ext`
+        self.library_dirs = self.include_dirs = [
+            os.path.join(self.build_temp, info["makedir"])
+            for lib, info in self.libraries
+        ]
 
     def run(self):
+        self.build_info = dict(self.libraries)
+        _build_clib.run(self)
+
+
+class clean(_clean):
+
+    def run(self):
+
         source_dir = os.path.join(os.path.dirname(__file__), "pyhmmer")
 
         patterns = ["*.html"]
@@ -121,8 +167,10 @@ class clean_ext(setuptools.Command):
 
         for pattern in patterns:
             for file in glob.glob(os.path.join(source_dir, pattern)):
-                self.announce("removing {!r}".format(file), level=2)
+                log.info("removing {!r}".format(file))
                 os.remove(file)
+
+        _clean.run(self)
 
 
 extensions = [
@@ -143,8 +191,28 @@ extensions = [
     ),
 ]
 
+# the libraries dictionary to use with the `build_clib` command
+# - sources (required by the base command): name of the autoconf template
+# - vendordir: where the vendored sources are located
+# - builddir: where to copy the vendored sources (with the temp folder as root)
+# - makedir: the path where to invoke `make <staticlib>`
+libraries = {
+    "easel": {
+        "sources": [],
+        "vendordir": os.path.join("vendor", "easel"),
+        "builddir": os.path.join("hmmer", "easel"),
+        "makedir": os.path.join("hmmer", "easel"),
+    },
+    "hmmer": {
+        "sources": [],
+        "vendordir": os.path.join("vendor", "hmmer"),
+        "builddir": "hmmer",
+        "makedir": os.path.join("hmmer", "src"),    #
+    },
+}
 
 setuptools.setup(
     ext_modules=cythonize(extensions, annotate=True, include_path=["include"]),
-    cmdclass=dict(sdist=sdist, build_ext=build_ext, clean_ext=clean_ext),
+    libraries=list(libraries.items()),
+    cmdclass=dict(sdist=sdist, build_ext=build_ext, clean=clean, build_clib=build_clib),
 )
