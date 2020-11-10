ac1c76b41123cb0efb95fe31eac68422b778bea6
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-10 14:39:52 +0100

    Register `easel` and `plan7` classes as some `collections.abc` subclasses

--- pyhmmer/__init__.py
+++ pyhmmer/__init__.py
@@ -5,7 +5,22 @@ __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __license__ = "MIT"
 __version__ = "0.1.0"
 
+import collections.abc as _collections_abc
+import contextlib as _contextlib
+
 from . import easel
 from . import plan7
 
 from .hmmsearch import hmmsearch
+
+
+# Register collections using the `collections.abc` module (this is probably
+# not required with later versions of Python)
+_collections_abc.Iterator.register(easel.SequenceFile)
+_collections_abc.Iterator.register(plan7.HMMFile)
+_collections_abc.Sized.register(plan7.Alignment)
+_collections_abc.Sequence.register(easel.Bitfield)
+_collections_abc.Sequence.register(plan7.Domains)
+_collections_abc.Sequence.register(plan7.TopHits)
+_contextlib.AbstractContextManager.register(easel.SequenceFile)
+_contextlib.AbstractContextManager.register(plan7.HMMFile)
