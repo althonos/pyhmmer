a3df4a0b8c2d84dfd9b74759984a68fa39caff18
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-21 02:32:41 +0100

    Ensure `hmmsearch` reports errors occuring in threads without deadlock

--- pyhmmer/hmmer.py
+++ pyhmmer/hmmer.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
+import ctypes
 import queue
+import time
 import threading
 import typing
 import multiprocessing
@@ -11,10 +13,17 @@ from .plan7 import Pipeline, TopHits, HMM, HMMFile
 
 class _PipelineThread(threading.Thread):
 
+    @staticmethod
+    def _none_callback(hmm: HMM, total: int) -> None:
+        pass
+
     def __init__(
         self,
         sequences: typing.Iterable[DigitalSequence],
         hmm_queue: "queue.Queue[typing.Optional[HMM]]",
+        hmm_count: multiprocessing.Value,  # type: ignore
+        kill_switch: threading.Event,
+        callback: typing.Optional[typing.Callable[[HMM, int], None]],
         options: typing.Dict[str, typing.Any],
     ) -> None:
         super().__init__()
@@ -23,39 +32,57 @@ class _PipelineThread(threading.Thread):
         self.hits = TopHits()
         self.sequences = sequences
         self.hmm_queue = hmm_queue
+        self.hmm_count = hmm_count
+        self.callback = callback or self._none_callback
+        self.kill_switch = kill_switch
+        self.error = None
 
     def run(self) -> None:
-        while True:
+        while not self.kill_switch.is_set():
             hmm = self.hmm_queue.get()
             if hmm is None:
                 self.hmm_queue.task_done()
                 return
             with self.pipeline_lock:
-                self.pipeline.search(hmm, self.sequences, hits=self.hits)
-            self.hmm_queue.task_done()
+                try:
+                    self.pipeline.search(hmm, self.sequences, hits=self.hits)
+                    self.hmm_queue.task_done()
+                    self.callback(hmm, self.hmm_count.value)
+                except BaseException as exc:
+                    self.error = exc
+                    self.kill()
+                    return
+
+    def kill(self):
+        self.kill_switch.set()
 
 
 def hmmsearch(
     hmms: typing.Iterable[HMM],
     sequences: typing.Sequence[DigitalSequence],
     cpus: int = 0,
+    callback: typing.Optional[typing.Callable[[HMM, int], None]] = None,
     **options: typing.Any,
 ) -> TopHits:
     # count the number of CPUs to use
     _cpus = cpus if cpus > 0 else multiprocessing.cpu_count()
 
-    # create the queue to pass the HMM objects around
+    # create the queues to pass the HMM objects around, as well as atomic
+    # values that we use to synchronize the threads
     hmm_queue = typing.cast("queue.Queue[typing.Optional[HMM]]", queue.Queue())
+    hmm_count = multiprocessing.Value(ctypes.c_ulong)
+    kill_switch = threading.Event()
 
     # create and launch one pipeline thread per CPU
     threads = []
     for _ in range(_cpus):
-        thread = _PipelineThread(sequences, hmm_queue, options)
+        thread = _PipelineThread(sequences, hmm_queue, hmm_count, kill_switch, callback, options)
         thread.start()
         threads.append(thread)
 
     # queue the HMMs passed as arguments
     for hmm in hmms:
+        hmm_count.value += 1
         hmm_queue.put(hmm)
 
     # poison-pill the queue so that threads terminate when they
@@ -63,8 +90,11 @@ def hmmsearch(
     for _ in threads:
         hmm_queue.put(None)
 
-    # wait for all HMMs to be processed
-    hmm_queue.join()
+    # wait for all threads to be completed
+    for thread in threads:
+        thread.join()
+        if thread.error is not None:
+            raise thread.error
 
     # merge the hits from each pipeline and return the merged hits
     hits = threads[0].hits
@@ -74,7 +104,7 @@ def hmmsearch(
     # extract the first pipeline from the thread so that the TopHits can use it,
     # and patch the Z value using either the options or the sequences count
     with threads[0].pipeline_lock:
-        hits.pipeline, threads[0].pipeline = threads[0].pipeline, None
+        hits.pipeline, threads[0].pipeline = threads[0].pipeline, None  # type: ignore
         hits.pipeline.Z = options.get("Z", len(sequences))
 
     # return thresholded hits
@@ -97,11 +127,17 @@ if __name__ == "__main__":
 
     with SequenceFile(args.seqdb) as seqfile:
         alphabet = seqfile.guess_alphabet()
+        if alphabet is None:
+            print("could not guess alphabet of input, exiting")
+            sys.exit(1)
+
         seq = TextSequence()
         sequences = []
         while seqfile.readinto(seq) is not None:
             sequences.append(seq.digitize(alphabet))
             seq.clear()
 
-    with HMMFile(args.hmmfile) as hmms:
-        hmmsearch(hmms, sequences, cpus=args.jobs)
+        with HMMFile(args.hmmfile) as hmms:
+            hits = hmmsearch(hmms, sequences, cpus=args.jobs)
+
+    print("Found {} hits".format(len(hits)))
