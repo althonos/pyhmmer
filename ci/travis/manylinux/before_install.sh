aa5640960b6d71aaaedc4e47934f8cd272a1d110
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-11-22 22:15:53 +0100

    Remove uneeded patching of HMMER sources in CI scripts

--- ci/travis/manylinux/before_install.sh
+++ ci/travis/manylinux/before_install.sh
@@ -9,7 +9,3 @@ log Launching \`manylinux\` docker container
 docker pull $IMG
 docker run -d -e TERM=$TERM -v $TRAVIS_BUILD_DIR:/io --name manylinux --rm -it $IMG sh
 
-
-# --- Fix HMMER makefile -----------------------------------------------------
-
-sed -i "s/= @CFLAGS@/= @CFLAGS@ @PIC_CFLAGS@/g" vendor/hmmer/src/impl_sse/Makefile.in
--- ci/travis/osx/before_install.sh
+++ ci/travis/osx/before_install.sh
@@ -3,6 +3,3 @@
 . $(dirname $(dirname $0))/functions.sh
 
 
-# --- Fix HMMER makefile -----------------------------------------------------
-
-sed -i.bak "s/= @CFLAGS@/= @CFLAGS@ @PIC_CFLAGS@/g" vendor/hmmer/src/impl_sse/Makefile.in
