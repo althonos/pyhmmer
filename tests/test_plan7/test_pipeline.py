4b435607715d97b9e5fab93e4cf8ae2adad7c821
Martin Larralde <martin.larralde@embl.de>
2021-01-19 14:01:40 +0100

    Add test to check `Pipeline.search_seq` raises a `ValueError` on alphabet mismatch

--- tests/test_plan7/test_pipeline.py
+++ tests/test_plan7/test_pipeline.py
@@ -12,13 +12,24 @@ from pyhmmer.plan7 import Background, Builder, Pipeline, HMMFile, TopHits
 from pyhmmer.easel import Alphabet, SequenceFile, TextSequence
 
 
-class TestPipelinesearch(unittest.TestCase):
+class TestPipeline(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.alphabet = Alphabet.amino()
         with SequenceFile(pkg_resources.resource_filename("tests", "data/seqs/938293.PRJEB85.HG003687.faa")) as f:
             cls.references = [x.digitize(cls.alphabet) for x in f]
+
+    def test_alphabet_mismatch(self):
+        pipeline = Pipeline(alphabet=Alphabet.dna())
+
+        # mismatch between pipeline alphabet and query alphabet
+        dsq = TextSequence(sequence="IRGIY").digitize(self.alphabet)
+        self.assertRaises(ValueError, pipeline.search_seq, dsq, self.references)
+
+        # mismatch between pipeline alphabet and database alphabet
+        dsq = TextSequence(sequence="ATGC").digitize(pipeline.alphabet)
+        self.assertRaises(ValueError, pipeline.search_seq, dsq, self.references)
 
     def test_search_hmm(self):
         seq = TextSequence(sequence="IRGIYNIIKSVAEDIEIGIIPPSKDHVTISSFKSPRIADT")
