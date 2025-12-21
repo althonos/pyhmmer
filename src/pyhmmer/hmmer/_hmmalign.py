ae938fd1ab59d056628da5794183af40f9331854
Martin Larralde <martin.larralde@embl.de>
2025-12-21 13:22:23 +0100

    Implement parallel processing with threads in `hmmalign`

--- src/pyhmmer/hmmer/_hmmalign.py
+++ src/pyhmmer/hmmer/_hmmalign.py
@@ -1,7 +1,26 @@
+import os
 import typing
+import functools
+import itertools
+from multiprocessing.pool import ThreadPool
+
+import psutil
 
 from ..easel import DigitalSequence, DigitalSequenceBlock, MSA
-from ..plan7 import HMM, TraceAligner
+from ..plan7 import HMM, TraceAligner, Traces
+
+if typing.TYPE_CHECKING:
+    from ._base import BACKEND
+
+# --- Utils ------------------------------------------------------------------
+
+def _batch_sequences(sequences: DigitalSequenceBlock, cpus: int) -> typing.Iterable[DigitalSequenceBlock]:
+    batchsize = (len(sequences) + cpus - 1) // cpus
+    indices = [0]
+    for i in range(cpus):
+        indices.append(indices[i] + batchsize)
+    indices[-1] = len(sequences)
+    return (sequences[indices[i]:indices[i+1]] for i in range(cpus))
 
 # --- hmmalign ---------------------------------------------------------------
 
@@ -9,6 +28,7 @@ def hmmalign(
     hmm: HMM,
     sequences: typing.Iterable[DigitalSequence],
     *,
+    cpus: int = 0,
     digitize: bool = False,
     trim: bool = False,
     all_consensus_cols: bool = True,
@@ -22,6 +42,12 @@ def hmmalign(
             sequences to align to the HMM. If you plan on using the
             same sequences several times, consider storing them into
             a `~pyhmmer.easel.DigitalSequenceBlock` directly.
+
+    Keyword Arguments:
+        cpus (`int`): The number of threads to run in parallel. Pass ``1``
+            to run everything in the main thread, ``0`` to automatically
+            select a suitable number (using `psutil.cpu_count`), or any
+            positive number otherwise.
         trim (`bool`): Trim off any residues that get assigned to
             flanking :math:`N` and :math:`C` states (in profile traces)
             or :math:`I_0` and :math:`I_m` (in core traces).
@@ -44,10 +70,25 @@ def hmmalign(
     .. versionadded:: 0.4.7
 
     """
+    cpus = cpus if cpus > 0 else psutil.cpu_count(logical=False) or os.cpu_count() or 1
+
     aligner = TraceAligner()
     if not isinstance(sequences, DigitalSequenceBlock):
         sequences = DigitalSequenceBlock(hmm.alphabet, sequences)
-    traces = aligner.compute_traces(hmm, sequences)
+
+    if len(sequences) > cpus:
+        cpus = len(sequences)
+
+    if cpus == 1:
+        traces = aligner.compute_traces(hmm, sequences)
+    else:
+        batches = _batch_sequences(sequences, cpus)
+        with ThreadPool(cpus) as pool:
+            traces = Traces()
+            _compute_traces = functools.partial(aligner.compute_traces, hmm)
+            for batch_traces in pool.map(_compute_traces, batches):
+                traces.extend(batch_traces)
+
     return aligner.align_traces(
         hmm,
         sequences,
