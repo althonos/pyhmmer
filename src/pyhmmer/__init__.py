d10ba9d4f5ddbbaf13aba0e2b939755d981dec15
Martin Larralde <martin.larralde@embl.de>
2026-01-17 12:47:33 +0100

    Add code to load DLLs from `pyhmmer.libs` on Windows

--- src/pyhmmer/__init__.py
+++ src/pyhmmer/__init__.py
@@ -27,7 +27,19 @@ References:
 """
 import collections.abc as _collections_abc
 import contextlib as _contextlib
-import os as _os
+
+
+def _patch_dll():
+    import os
+
+    if hasattr(os, "add_dll_directory"):
+        libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pyhmmer.libs'))
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+
+_patch_dll()
+del _patch_dll
+
 
 from . import errors
 from . import easel
@@ -35,7 +47,6 @@ from . import plan7
 from . import daemon
 from .easel import __version__
 from .hmmer import hmmalign, hmmsearch, hmmpress, nhmmer, hmmscan, phmmer, jackhmmer
-
 
 __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __license__ = "MIT"
